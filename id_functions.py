db11230c0a7bffdcd00fb57695f9114f86aa80e1
Artem Korolev <92857557+ArKorolev@users.noreply.github.com>
2022-10-16 20:08:58 +0500

    Update id_functions.py

--- id_functions.py
+++ id_functions.py
@@ -3,14 +3,12 @@ import collections
 
 def group(client_id):
     """определяет номер группы клиентов по сумме цифр их ID"""
-    total = 0  # счетчик суммы цифр числа
-    while client_id > 0:  # разложение числа на цифры и их суммирование
-        total += client_id % 10  # остаток от деления числа является его последней цифрой
-        client_id //= 10  # убираем последнюю цифру в числе
+    total = 0
+    while client_id > 0:
+        total += client_id % 10
+        client_id //= 10
     return total
 
-
-print(f"клиент с ID 7412567 в группе {group(7412567)}")  # проверка функции, ответ 32
 
 client_list = [_ for _ in range(10000, 10000000)]  # список 5-7 значных ID
 group_list = []  # преобразование ID в номер группы
